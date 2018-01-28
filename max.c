20e5201f5a3d9c974d1654eb42ba9274cffbcdef
verma18 <dev97verma@gmail.com>
2018-01-29 00:28:59 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{int n,i,max,arr[10000];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+max=arr[0];
+for(i=0;i<n;i++)
+{
+    if(max<arr[i])
+        max=arr[i];
+}
+printf("%d",max);
+
+}
